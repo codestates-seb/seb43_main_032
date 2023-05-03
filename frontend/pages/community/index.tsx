d605949534ae1a016ce142412f209f75d838d758
GiRang Choi <choigirang@kakao.com>
2023-05-04 03:35:27 +0900

    feat: SideBar & Content 그리드 적용

--- frontend/pages/community/index.tsx
+++ frontend/pages/community/index.tsx
@@ -1,6 +1,15 @@
+import GridBox from '@/components/GridBox';
+import Content from '@/components/community/Content';
+import SideBar from '@/components/community/SideBar';
+
 //커뮤니티 홈 페이지 입니다. 경로 '/community/'
 const Community = () => {
-  return <div>커뮤니티</div>;
+  return (
+    <GridBox>
+      <SideBar />
+      <Content />
+    </GridBox>
+  );
 };
 
 export default Community;
