b7f5dc6e3a5fdcff787d2a247044583efe299505
tree4553 <tree4553@naver.com>
2023-05-03 15:51:11 +0900

    feat : 초기 개발 환경 세팅

---
+++ backend/sidequest/src/main/java/com/main_032/sidequest/SidequestApplication.java
@@ -0,0 +1,13 @@
+package com.main_032.sidequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidequestApplication.class, args);
+	}
+
+}
---
+++ backend/sidequest/src/test/java/com/main_032/sidequest/SidequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main_032.sidequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
