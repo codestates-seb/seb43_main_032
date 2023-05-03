d140d9bf550e837c9ce028b615661af039bbfa06
GiRang Choi <choigirang@kakao.com>
2023-05-04 03:30:47 +0900

    feat: community 페이지 글 목록 임시 작성

--- frontend/__mocks__/api.ts
+++ frontend/__mocks__/api.ts
@@ -8,4 +8,158 @@ export const handlers = [
   //   const data = req.json();
   //   return res(ctx.status(200), ctx.json({ result: data }));
   // }),
+
+  rest.get('/posts', async (req, res, ctx) => {
+    const example = [
+      {
+        page: 1,
+        content: [
+          {
+            id: 'example',
+            user: 'exms2837',
+            score: 5,
+            star: 1024,
+            view: 594,
+            title: 'React 작성 시 TypeScript 질문입니다.',
+            tags: ['React', 'TypeScript', 'js'],
+            content:
+              'lorem lipsum lorem lipsumlorem lipsumlorem lipsum lorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsum',
+          },
+          {
+            id: 'example',
+            user: 'exms2837',
+            score: 5,
+            star: 1024,
+            view: 594,
+            title: 'React 작성 시 TypeScript 질문입니다.',
+            tags: ['React', 'TypeScript', 'js'],
+            content:
+              'lorem lipsum lorem lipsumlorem lipsumlorem lipsum lorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsum',
+          },
+          {
+            id: 'example',
+            user: 'exms2837',
+            score: 5,
+            star: 1024,
+            view: 594,
+            title: 'React 작성 시 TypeScript 질문입니다.',
+            tags: ['React', 'TypeScript', 'js'],
+            content:
+              'lorem lipsum lorem lipsumlorem lipsumlorem lipsum lorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsum',
+          },
+          {
+            id: 'example',
+            user: 'exms2837',
+            score: 5,
+            star: 1024,
+            view: 594,
+            title: 'React 작성 시 TypeScript 질문입니다.',
+            tags: ['React', 'TypeScript', 'js'],
+            content:
+              'lorem lipsum lorem lipsumlorem lipsumlorem lipsum lorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsum',
+          },
+          {
+            id: 'example',
+            user: 'exms2837',
+            score: 5,
+            star: 1024,
+            view: 594,
+            title: 'React 작성 시 TypeScript 질문입니다.',
+            tags: ['React', 'TypeScript', 'js'],
+            content:
+              'lorem lipsum lorem lipsumlorem lipsumlorem lipsum lorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsum',
+          },
+          {
+            id: 'example',
+            user: 'exms2837',
+            score: 5,
+            star: 1024,
+            view: 594,
+            title: 'React 작성 시 TypeScript 질문입니다.',
+            tags: ['React', 'TypeScript', 'js'],
+            content:
+              'lorem lipsum lorem lipsumlorem lipsumlorem lipsum lorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsum',
+          },
+          {
+            id: 'example',
+            user: 'exms2837',
+            score: 5,
+            star: 1024,
+            view: 594,
+            title: 'React 작성 시 TypeScript 질문입니다.',
+            tags: ['React', 'TypeScript', 'js'],
+            content:
+              'lorem lipsum lorem lipsumlorem lipsumlorem lipsum lorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsum',
+          },
+          {
+            id: 'example',
+            user: 'exms2837',
+            score: 5,
+            star: 1024,
+            view: 594,
+            title: 'React 작성 시 TypeScript 질문입니다.',
+            tags: ['React', 'TypeScript', 'js'],
+            content:
+              'lorem lipsum lorem lipsumlorem lipsumlorem lipsum lorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsumlorem lipsum lorem lipsumlorem lipsumlorem lipsum',
+          },
+        ],
+      },
+      {
+        page: 2,
+        content: [
+          {
+            id: 'example',
+            user: 'exms2837',
+            score: 5,
+            star: 1024,
+            view: 594,
+            title: 'React 작성 시 TypeScript 질문입니다.',
+            tags: ['React', 'TypeScript', 'js'],
+            content: 'lorem lipsum lorem lipsumlorem lipsumlorem lipsum',
+          },
+          {
+            id: 'example',
+            user: 'exms2837',
+            score: 5,
+            star: 1024,
+            view: 594,
+            title: 'React 작성 시 TypeScript 질문입니다.',
+            tags: ['React', 'TypeScript', 'js'],
+            content: 'lorem lipsum lorem lipsumlorem lipsumlorem lipsum',
+          },
+          {
+            id: 'example',
+            user: 'exms2837',
+            score: 5,
+            star: 1024,
+            view: 594,
+            title: 'React 작성 시 TypeScript 질문입니다.',
+            tags: ['React', 'TypeScript', 'js'],
+            content: 'lorem lipsum lorem lipsumlorem lipsumlorem lipsum',
+          },
+          {
+            id: 'example',
+            user: 'exms2837',
+            score: 5,
+            star: 1024,
+            view: 594,
+            title: 'React 작성 시 TypeScript 질문입니다.',
+            tags: ['React', 'TypeScript', 'js'],
+            content: 'lorem lipsum lorem lipsumlorem lipsumlorem lipsum',
+          },
+          {
+            id: 'example',
+            user: 'exms2837',
+            score: 5,
+            star: 1024,
+            view: 594,
+            title: 'React 작성 시 TypeScript 질문입니다.',
+            tags: ['React', 'TypeScript', 'js'],
+            content: 'lorem lipsum lorem lipsumlorem lipsumlorem lipsum',
+          },
+        ],
+      },
+    ];
+    return res(ctx.status(200), ctx.json({ example }));
+  }),
 ];
