12ac5d3953d75d7913d69bed55ab82a73cb806ae
GiRang Choi <choigirang@kakao.com>
2023-05-05 01:30:45 +0900

    feat: 동적 라우팅 페이지 작성 중

--- frontend/pages/community/[id]/index.tsx
+++ frontend/pages/community/[id]/index.tsx
@@ -1,5 +1,6 @@
 import GridBox from '@/components/GridBox';
 import Content from '@/components/community/Content';
+import PostPage from '@/components/community/PostPage';
 import SideBar from '@/components/community/SideBar';
 
 //프로젝트 조회 페이지 입니다. 경로 '/community/[id]'  예시 >> /community/3
@@ -7,7 +8,7 @@ const ViewCommunity = () => {
   return (
     <GridBox>
       <SideBar />
-      <Content />
+      <PostPage />
     </GridBox>
   );
 };
