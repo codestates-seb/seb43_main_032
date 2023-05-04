2c1c8a84a6469a9d0b54ceebd33c7f83fd413e26
GiRang Choi <choigirang@kakao.com>
2023-05-04 16:46:45 +0900

    feat: 페이지 네이션 작성 중

--- frontend/components/community/ContentPageNation.tsx
+++ frontend/components/community/ContentPageNation.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import { api } from '@/util/api';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 export default function ContentPageNation() {
-  return <div>ContentPageNation</div>;
+  const [pageNum, setPageNum] = useState([]);
+
+  useEffect(() => {
+    getPageNum();
+  }, []);
+
+  const getPageNum = async () => {
+    await api.get('/posts').then((res) => {
+      setPageNum(res.data.example);
+      console.log(res.data.example);
+    });
+  };
+
+  return (
+    <Container>
+      <PageContainer>
+        {pageNum.map((el, idx) => (
+          <div key={idx}>{idx}</div>
+        ))}
+      </PageContainer>
+    </Container>
+  );
+  // pagenation 추가~
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PageContainer = styled.div`
+  display: flex;
+`;
