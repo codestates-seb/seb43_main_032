acab6be6d200c1f4fb3feb9004d8284c9d94a11a
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-05-06 00:08:37 +0900

    feat: postpage 작성 예정

--- frontend/pages/community/post/[id].tsx
+++ frontend/pages/community/post/[id].tsx
@@ -1,15 +1,24 @@
 import GridBox from '@/components/GridBox';
 import PostAddPage from '@/components/community/PostAddPage';
 import SideBar from '@/components/community/SideBar';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
 
 //커뮤니티 글 작성 페이지 입니다. 경로 '/community/create/'
 const CommunityPostPage = () => {
+  const router = useRouter();
+  const idNum = router.query.id;
+
   return (
     <GridBox>
       <SideBar />
-      <PostAddPage />
+      <TextSize>{idNum}</TextSize>
     </GridBox>
   );
 };
+
+const TextSize = styled.div`
+  font-size: 50px;
+`;
 
 export default CommunityPostPage;
