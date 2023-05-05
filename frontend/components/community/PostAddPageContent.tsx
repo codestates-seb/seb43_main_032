9249d3c4866829a81c94c85be7f0b05c5b48e82b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-05-06 00:07:56 +0900

    feat: useForm 사용 예정 주석 처리

--- frontend/components/community/PostAddPageContent.tsx
+++ frontend/components/community/PostAddPageContent.tsx
@@ -1,36 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import styled from 'styled-components';
+import MainPost from '../MainPost';
+import { DefaultObj } from '@/types/types';
 
-type FormData = {
-  name: string;
-  email: string;
+const postCommunity = () => {
+  // const data = {
+  //   start,
+  //   end,
+  //   select,
+  //   tags,
+  //   job,
+  //   position: watch().position,
+  //   title: watch().title,
+  //   editor,
+  // };
+  //   api.post('/project', data).then(() => router.push('/'));
 };
 
 export default function PostAddPageContent() {
-  const { register, handleSubmit, errors } = useForm<FormData>();
-  const onSubmit: SubmitHandler<FormData> = (data) => console.log(data);
+  const { register, watch, reset } = useForm<DefaultObj>();
+
+  const [editor, setEditor] = useState('');
+  const changeEditor = (value: string) => {
+    setEditor(value);
+  };
+
+  const submitFun = () => {
+    postCommunity();
+  };
 
   return (
     <Container>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="name">Name</label>
-        <input id="name" name="name" ref={register({ required: true })} />
-        {errors.name && <span>This field is required</span>}
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          name="email"
-          ref={register({ required: true, pattern: /^\S+@\S+$/i })}
-        />
-        {errors.email && <span>Please enter a valid email address</span>}
-        <button type="submit">Submit</button>
-      </form>
+      <MainPost type={2} register={register} changeEditor={changeEditor} />
+      <SubmitBtn onSubmit={submitFun}>제출하기</SubmitBtn>
     </Container>
   );
 }
 
 const Container = styled.div`
   width: 100%;
-  height: 500px;
+  display: flex;
+  flex-direction: column;
+  /* padding: var(--padding-2); */
+`;
+
+const SubmitBtn = styled.button`
+  width: 20%;
+  margin: var(--padding-2);
+  margin-top: 0;
+  line-height: 2rem;
+  border: none;
+  background: #5959cb;
+  color: white;
+  border-radius: var(--radius-sm);
+
+  &:hover {
+    background-color: #242468;
+    transform: scale(1.1);
+  }
 `;
