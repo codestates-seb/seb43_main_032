10216376c93add8d76690c88ce1c732612dac493
Latemarch <dolphin0331@naver.com>
2023-05-06 17:22:04 +0900

    Add padding on _app.tsx
    헤더 아랫부분에 헤더 높이만큼 padding-top: 80px 추가했습니다.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,6 +3,7 @@ import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import '../styles/App.css';
 import Header from '../components/Header';
+import styled from 'styled-components';
 
 const queryClient = new QueryClient();
 
@@ -10,12 +11,18 @@ if (process.env.NODE_ENV === 'development') {
   require('../__mocks__');
 }
 
+const Main = styled.div`
+  padding-top: 80px;
+`;
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <Header />
-        <Component {...pageProps} />
+        <Main>
+          <Component {...pageProps} />
+        </Main>
       </RecoilRoot>
     </QueryClientProvider>
   );
