172287850b5db0123c5629f6ed2d94cb9f8984a4
Latemarch <73418661+Latemarch@users.noreply.github.com>
2023-05-07 01:10:04 +0900

    Add edit

--- frontend/components/user/InfoContainer.tsx
+++ frontend/components/user/InfoContainer.tsx
@@ -1,14 +1,18 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import styled from 'styled-components';
+import AuthInput from '../authAction/AuthInput';
 
 const Wrapper = styled.div`
-  border-bottom: ${({ lastItem }: { lastItem: Boolean }) =>
-    lastItem ? '' : '1px solid rgba(0, 0, 0, 0.3)'};
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding-top: 20px;
-  padding-bottom: 20px;
+  font-size: 15px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  ${({ lastItem }: { lastItem: Boolean }) =>
+    lastItem
+      ? 'padding-bottom: 0px'
+      : 'border-bottom: 1px solid rgba(0, 0, 0, 0.3)'}
 `;
 const Leftbox = styled.div`
   display: flex;
@@ -16,7 +20,7 @@ const Leftbox = styled.div`
   gap: 20px;
 `;
 const KeyContainer = styled.div`
-  width: 120px;
+  width: 100px;
 `;
 const ContentContainer = styled.div`
   display: flex;
@@ -24,32 +28,88 @@ const ContentContainer = styled.div`
   flex-direction: column;
   justify-content: flex-start;
 `;
-const Button = styled.button`
+const Button = styled.button.attrs({
+  className: 'noto-regular',
+})`
+  margin-left: 10px;
+  padding: 10px;
   border: none;
   cursor: pointer;
-  padding: 10px 20px;
   font-size: 20px;
   border-radius: 5px;
 `;
-
+const Input = styled.input`
+  /* position: relative; */
+  /* width: 100%; */
+  border: none;
+  height: 40px;
+  margin-bottom: 7px;
+  border-radius: 10px;
+  padding: 10px;
+  --tw-ring-inset: var(--tw-empty, /*!*/ /*!*/);
+  --tw-ring-offset-width: 0px;
+  --tw-ring-offset-color: #fff;
+  --tw-ring-color: rgba(59, 130, 246, 0.5);
+  --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0
+    var(--tw-ring-offset-width) var(--tw-ring-offset-color);
+  --tw-ring-shadow: var(--tw-ring-inset) 0 0 0
+    calc(1px + var(--tw-ring-offset-width)) var(--tw-ring-color);
+  box-shadow: var(--tw-ring-offset-shadow), var(--tw-ring-shadow),
+    var(--tw-shadow, 0 0 #0000);
+  &:focus {
+    outline: none;
+    --tw-ring-color: rgba(141, 184, 252, 0.3);
+    --tw-ring-shadow: var(--tw-ring-inset) 0 0 0
+      calc(5px + var(--tw-ring-offset-width)) var(--tw-ring-color);
+    box-shadow: var(--tw-ring-offset-shadow), var(--tw-ring-shadow),
+      var(--tw-shadow, 0 0 #0000);
+  }
+`;
+interface IProps {
+  name: string;
+  keyNode: ReactNode;
+  contentNode: any;
+  lastItem?: Boolean;
+}
 export default function InfoContainer({
+  name,
   keyNode,
   contentNode,
   lastItem = false,
-}: {
-  keyNode: ReactNode;
-  contentNode: ReactNode;
-  lastItem?: Boolean;
-}) {
+}: IProps) {
+  const [isEdit, setIsEdit] = useState<Boolean>(false);
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setIsEdit((prev) => !prev);
+  };
   return (
     <Wrapper lastItem={lastItem}>
       <Leftbox>
-        <KeyContainer className="nanum-bold">{keyNode}</KeyContainer>
-        <ContentContainer className="nanum-bold">
-          {contentNode}
-        </ContentContainer>
+        <KeyContainer className="nnum-bold">{keyNode}</KeyContainer>
+        {name !== 'profile' && isEdit ? (
+          <Input
+            placeholder={contentNode ? contentNode : 'input..'}
+            type={name}
+          />
+        ) : (
+          <ContentContainer className="nnum-bold">
+            {contentNode}
+          </ContentContainer>
+        )}
       </Leftbox>
-      <Button>Edit</Button>
+      {isEdit ? (
+        <div className="noto-regular">
+          <Button data-name="Cancel" onClick={handleClick}>
+            Cancel
+          </Button>
+          <Button data-name="Submit" onClick={handleClick}>
+            Submit
+          </Button>
+        </div>
+      ) : (
+        <Button data-name="Edit" onClick={handleClick}>
+          Edit
+        </Button>
+      )}
     </Wrapper>
   );
 }
