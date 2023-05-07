18ebfea50cfb80cb7358ff0819c688839d2a35dd
shdomi8599 <shdomi@naver.com>
2023-05-08 01:55:02 +0900

    feat : 무한스크롤

--- frontend/pages/project/index.tsx
+++ frontend/pages/project/index.tsx
@@ -7,34 +7,59 @@ import { api } from '@/util/api';
 import { Project } from '@/types/types';
 import Loading from '@/components/Loading';
 import Error from '@/components/Error';
-import { useState, useRef, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 
 const ProjectHome = () => {
   const router = useRouter();
-  const target = useRef<HTMLDivElement>(null);
 
-  const [page, setPage] = useState(1);
+  //데이터 fetch
   const PAGE_LIMIT = 4;
   const { isLoading, error, data, fetchNextPage, hasNextPage } =
     useInfiniteQuery(
       'projects',
-      () =>
-        api(`${router.asPath}?size=${PAGE_LIMIT}&page=${page}`).then(
+      ({ pageParam = 1 }) =>
+        api(`${router.asPath}?size=${PAGE_LIMIT}&page=${pageParam}`).then(
           (res) => res.data
         ),
       {
         getNextPageParam: (lastPage, allPages) => {
           if (lastPage.data.length < PAGE_LIMIT) {
-            return null; // 모든 데이터를 가져왔으므로 null을 반환하여 더 이상 페이지를 로드하지 않음
+            return null;
           }
-          return allPages.length + 1; // 다음 페이지를 로드하기 위해 페이지 번호를 반환
+          return allPages.length + 1;
         },
       }
     );
-  console.log(data);
 
-  const projectList = data?.pages.flatMap((page) => page.data) ?? [];
-  console.log(projectList);
+  //무한 스크롤 effect
+  const target = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (
+      target.current &&
+      data?.pageParams[data.pageParams.length - 1] === null
+    ) {
+      return;
+    }
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          fetchNextPage();
+        }
+      },
+      { threshold: 1 }
+    );
+
+    if (target.current) {
+      observer.observe(target.current);
+    }
+
+    return () => {
+      if (target.current) {
+        observer.unobserve(target.current);
+      }
+    };
+  }, [target.current, data?.pageParams]);
+
   if (isLoading) return <Loading />;
   if (error) return <Error>잠시 후 다시 시도해주세요.</Error>;
   if (data)
@@ -58,13 +83,18 @@ const ProjectHome = () => {
           <div className="nanum-bold">전체 프로젝트</div>
           <div className="projects-box">
             {data.pages.map((page) =>
-              projectList.map((project: Project) => (
+              page.data.map((project: Project) => (
                 <ProjectCard key={project.id} size={'sm'} data={project} />
               ))
             )}
           </div>
         </div>
         <div ref={target} className="observer"></div>
+        {!hasNextPage && (
+          <div className="last-box nanum-bold blink">
+            페이지가 존재하지 않습니다.
+          </div>
+        )}
       </Box>
     );
 };
@@ -117,5 +147,12 @@ const Box = styled.div`
 
   .observer {
     height: 5vh;
+  }
+
+  .last-box {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 40px;
   }
 `;
