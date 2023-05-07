fc3bab6b57e54b1722ad934727c7c24f6f38665b
GiRang Choi <choigirang@kakao.com>
2023-05-08 01:45:57 +0900

    feat: 주석 추가 및 타이틀 검색 및 초기화 버튼,기능 추가

--- frontend/components/community/ContentTop.tsx
+++ frontend/components/community/ContentTop.tsx
@@ -1,17 +1,24 @@
-import { searchState } from '@/recoil/atom';
+import { resetSearchState, searchState } from '@/recoil/atom';
 import React, { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
 import { useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
 
+// item 타이틀 검색어 기능
 export default function ContentTop() {
+  // 입력어 저장
   const [matchTitle, setMatchTitle] = useState('');
-  const saveSearchTitle = useSetRecoilState(searchState);
 
+  // 입력어 전역 저장
+  const saveSearchTitle = useSetRecoilState(searchState);
+  const resetSearch = useSetRecoilState(resetSearchState);
+
+  // 입력된 타이틀로 저장
   const findContentItem = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMatchTitle(e.target.value);
-    console.log(matchTitle);
   };
 
+  // 입력어 저장시키고 초기화
   const handleSearch = () => {
     saveSearchTitle(matchTitle);
     setMatchTitle('');
@@ -23,8 +30,20 @@ export default function ContentTop() {
         placeholder="검색어를 입력하세요."
         value={matchTitle}
         onChange={findContentItem}
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            handleSearch();
+          }
+        }}
       />
-      <SearchBtn onClick={handleSearch} />
+      <SearchBtn
+        onClick={() => {
+          handleSearch(), resetSearch(true);
+        }}
+      >
+        <FaSearch />
+      </SearchBtn>
+      <SearchBtn onClick={() => resetSearch(false)}>초기화</SearchBtn>
     </Container>
   );
 }
@@ -39,12 +58,12 @@ const Container = styled.div`
 
 const SearchInput = styled.input`
   width: 60%;
-  height: 40%;
   box-shadow: 2px 2px 7px 1px rgba(114, 114, 114, 0.75);
-  border-radius: var(--radius-def);
+  border-radius: 4px;
   border: none;
   margin-right: 16px;
-  padding: 5px 10px;
+  padding: 10px 22px;
+  color: #5393fa;
   &:focus,
   :active {
     outline: none;
@@ -54,15 +73,19 @@ const SearchInput = styled.input`
 `;
 
 const SearchBtn = styled.button`
-  width: 10%;
-  height: 40%;
-  box-shadow: 2px 2px 7px 1px rgba(114, 114, 114, 0.75);
-  border-radius: var(--radius-def);
+  border-radius: 4px;
+  background: #96bfff;
+  padding: 10px 22px;
+  color: #fff;
+  outline: none;
   border: none;
-  background: white;
   cursor: pointer;
-
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  margin-left: 12px;
   &:hover {
-    background-color: #6e6e6e;
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #010606;
   }
 `;
