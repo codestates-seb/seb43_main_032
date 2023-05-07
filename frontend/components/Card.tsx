8bdbf0c760cd9ebd2c8bcafd7fa4dbe931bfd9bc
shdomi8599 <shdomi@naver.com>
2023-05-07 18:53:40 +0900

    feat : Card 컴포넌트

---
+++ frontend/components/Card.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+
+type Props = React.HTMLAttributes<HTMLDivElement> & {
+  children: ReactNode;
+  width: number;
+};
+
+const Card = (props: Props) => <Box {...props} />;
+
+export default Card;
+
+const Box = styled.div<Props>`
+  padding: var(--padding-1);
+  width: ${(props) => `${props.width}%`};
+  border: 1px solid #e4e4e4;
+  border-radius: var(--radius-def);
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
--- frontend/pages/project/index.tsx
+++ frontend/pages/project/index.tsx
@@ -1,3 +1,4 @@
+import Card from '@/components/Card';
 import styled from 'styled-components';
 
 const Project = () => {
@@ -6,11 +7,29 @@ const Project = () => {
       <div className="special-box">
         <div>
           <div className="nanum-bold">신규 프로젝트</div>
-          <div className="card-box">zz</div>
+          <div className="card-box">
+            <Card width={75}>
+              <div className="img-box">
+                <img
+                  src="https://d1.awsstatic.com/glbl-digital-partner-marketing-fy22/logo-lockups/AWS_Salesforce_Logo-Lockup.d9894485fb0816380c1952aea58cc679628ee35d.png"
+                  alt=""
+                />
+              </div>
+              <div>제목</div>
+              <div>작성자</div>
+              <div>태그</div>
+              <div>스택 모집정원</div>
+              <div>view</div>
+            </Card>
+          </div>
         </div>
         <div>
           <div className="nanum-bold">인기 프로젝트</div>
-          <div className="card-box">zzz</div>
+          <div className="card-box">
+            <Card width={75}>
+              <div></div>
+            </Card>
+          </div>
         </div>
       </div>
       <div className="common-box">
@@ -24,6 +43,7 @@ const Project = () => {
 export default Project;
 
 const Box = styled.div`
+  padding: var(--padding-1);
   .special-box {
     width: 100%;
     display: grid;
@@ -36,6 +56,17 @@ const Box = styled.div`
       justify-content: center;
       align-items: center;
       margin: 24px 0px;
+      .img-box {
+        height: 120px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        > img {
+          width: 100%;
+          height: 100%;
+          border: 1px solid #e4e4e4;
+        }
+      }
     }
   }
 
