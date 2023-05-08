d36f43e67b2fc6725f8eba8f83f9632ebda0bbed
GiRang Choi <choigirang@kakao.com>
2023-05-08 15:39:45 +0900

    feat: height 값 제거

--- frontend/pages/community/create.tsx
+++ frontend/pages/community/create.tsx
@@ -1,17 +1,14 @@
 import GridBox from '@/components/GridBox';
 import PostAddPage from '@/components/community/PostAddPage';
 import SideBar from '@/components/community/SideBar';
-import { MinusTop } from '.';
 
 //커뮤니티 글 작성 페이지 입니다. 경로 '/community/create/'
 const CreateCommunity = () => {
   return (
-    <MinusTop>
-      <GridBox>
-        <SideBar />
-        <PostAddPage />
-      </GridBox>
-    </MinusTop>
+    <GridBox>
+      <SideBar />
+      <PostAddPage />
+    </GridBox>
   );
 };
 
--- frontend/pages/community/index.tsx
+++ frontend/pages/community/index.tsx
@@ -6,17 +6,13 @@ import styled from 'styled-components';
 //커뮤니티 홈 페이지 입니다. 경로 '/community/'
 const Community = () => {
   return (
-    <MinusTop>
-      <GridBox>
-        <SideBar />
-        <Content />
-      </GridBox>
-    </MinusTop>
+    <GridBox>
+      <SideBar />
+      <Content />
+    </GridBox>
   );
 };
 
 export default Community;
 
-export const MinusTop = styled.div`
-  transform: translateY(80px);
-`;
+export const MinusTop = styled.div``;
