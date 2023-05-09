47a4935912f493e565f26e572269acc408a533f5
GiRang Choi <choigirang@kakao.com>
2023-05-09 23:08:21 +0900

    feat: 페이지네이션 구현

--- frontend/components/community/ContentPageNation.tsx
+++ frontend/components/community/ContentPageNation.tsx
@@ -1,34 +1,76 @@
 import { api } from '@/util/api';
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-// 임시
-// 작성 중
-const getPageNum = async (
-  setPageNum: React.Dispatch<React.SetStateAction<never[]>>
-) => {
-  await api.get('/post').then((res) => {
-    setPageNum(res.data);
-  });
-};
+interface ContentPageNationProps {
+  totalData: number;
+  currentPage: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+}
 
-export default function ContentPageNation() {
-  const [pageNum, setPageNum] = useState([]);
+export default function ContentPageNation({
+  totalData,
+  currentPage,
+  setPage,
+}: ContentPageNationProps) {
+  const router = useRouter();
+  const [pageNum, setPageNum] = useState<number[]>([]);
 
   useEffect(() => {
-    getPageNum(setPageNum);
-  }, []);
+    const pageCount = Math.ceil(totalData / 10);
+    const arr = [];
+    for (let i = 1; i <= pageCount; i++) {
+      arr.push(i);
+    }
+    setPageNum(arr);
+  }, [totalData]);
+
+  const getPageNumsToShow = () => {
+    if (pageNum.length <= 7) {
+      return pageNum;
+    }
+
+    if (currentPage < 4) {
+      return [...pageNum.slice(0, 4), '...', pageNum.length];
+    }
+
+    if (currentPage > pageNum.length - 3) {
+      return [1, '...', ...pageNum.slice(-4)];
+    }
+
+    return [
+      1,
+      '...',
+      currentPage - 2,
+      currentPage - 1,
+      currentPage,
+      currentPage + 1,
+      currentPage + 2,
+      '...',
+      pageNum.length,
+    ];
+  };
 
   return (
     <Container>
       <PageContainer>
-        {pageNum.map((el, idx) => (
-          <div key={idx}>{idx}</div>
-        ))}
+        {getPageNumsToShow().map((el, idx) =>
+          el === '...' ? (
+            <PageEllipsis key={idx}>...</PageEllipsis>
+          ) : (
+            <PageButton
+              key={idx}
+              active={el === currentPage}
+              onClick={() => setPage(Number(el))}
+            >
+              {el}
+            </PageButton>
+          )
+        )}
       </PageContainer>
     </Container>
   );
-  // pagenation 추가~
 }
 
 const Container = styled.div`
@@ -39,4 +81,23 @@ const Container = styled.div`
 
 const PageContainer = styled.div`
   display: flex;
+`;
+
+const PageButton = styled.button<{ active: boolean }>`
+  color: ${({ active }) => (active ? '#256ce1' : 'gray')};
+  font-weight: ${({ active }) => (active ? 'bold' : '')};
+  margin: 0 2px;
+  padding: 4px;
+  border: solid 2px lightgray;
+  border-radius: 4px;
+  font-size: 15px;
+  cursor: pointer;
+
+  &:hover {
+    color: white;
+  }
+`;
+
+const PageEllipsis = styled.span`
+  margin: 0 0.5rem;
 `;
