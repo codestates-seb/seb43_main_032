e8c5842434349b451668ecdaf0884f5c36e3f56e
GiRang Choi <choigirang@kakao.com>
2023-05-11 02:17:13 +0900

    feat: post/:id 페이지 컴포넌트화

--- frontend/pages/community/post/[id].tsx
+++ frontend/pages/community/post/[id].tsx
@@ -1,27 +1,8 @@
-import GridBox from '@/components/GridBox';
-import PostAddPage from '@/components/community/PostAddPage';
-import SideBar from '@/components/community/SideBar';
-import { useRouter } from 'next/router';
-import styled from 'styled-components';
-import { MinusTop } from '..';
+import PostPage from '@/components/community/PostPage';
 
 //커뮤니티 글 작성 페이지 입니다. 경로 '/community/create/'
 const CommunityPostPage = () => {
-  const router = useRouter();
-  const idNum = router.query.id;
-
-  return (
-    <MinusTop>
-      <GridBox>
-        <SideBar />
-        <TextSize>{idNum}</TextSize>
-      </GridBox>
-    </MinusTop>
-  );
+  return <PostPage />;
 };
-
-const TextSize = styled.div`
-  font-size: 50px;
-`;
 
 export default CommunityPostPage;
