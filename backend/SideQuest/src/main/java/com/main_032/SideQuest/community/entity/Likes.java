e471202b87d4abe07f9de499ffd0192748b2b75a
tree4553 <tree4553@naver.com>
2023-05-10 17:06:20 +0900

    fix : Like 클래스 이름 Likes로 변경, yml 파일 아이디, 비밀번호, jwt key 환경변수 처리

--- backend/SideQuest/src/main/java/com/main_032/SideQuest/community/entity/Likes.java
+++ backend/SideQuest/src/main/java/com/main_032/SideQuest/community/entity/Likes.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 
 @Entity
 @Getter
-public class Like extends BaseEntity {
+public class Likes extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
