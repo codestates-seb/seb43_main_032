c6e802ebe99266b653ce67733e51e7db57944527
Yoon SeungHyun <dbstmdgus5050@gmail.com>
2023-05-11 15:21:57 +0900

    feat. swagger 추가

--- backend/SideQuest/src/main/java/com/main_032/SideQuest/member/controller/MemberController.java
+++ backend/SideQuest/src/main/java/com/main_032/SideQuest/member/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.main_032.SideQuest.member.controller;
 
 import com.main_032.SideQuest.member.dto.MemberPostDto;
 import com.main_032.SideQuest.member.service.MemberService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,17 +11,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = {"Member"})
 public class MemberController {
+
     private final MemberService memberService;
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
+    @ApiOperation(value = "Sign up") //회원가입
     @PostMapping("/signup")
-    public ResponseEntity signup(@RequestBody MemberPostDto memberPostDto) {
+    public ResponseEntity<Void> signup(@RequestBody MemberPostDto memberPostDto) {
         memberService.signup(memberPostDto);
-        ResponseEntity responseEntity = new ResponseEntity(HttpStatus.OK);
-        return responseEntity;
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
--- backend/SideQuest/src/main/java/com/main_032/SideQuest/member/dto/MemberPostDto.java
+++ backend/SideQuest/src/main/java/com/main_032/SideQuest/member/dto/MemberPostDto.java
@@ -1,10 +1,18 @@
 package com.main_032.SideQuest.member.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
-
 @Getter
+@ApiModel(description = "Data Transfer Object for Member Post", value = "MemberPostDto")
 public class MemberPostDto {
+
+    @ApiModelProperty(value = "Member's email", required = true)
     private String email;
+
+    @ApiModelProperty(value = "Member's name", required = true)
     private String name;
+
+    @ApiModelProperty(value = "Member's password", required = true)
     private String password;
 }
