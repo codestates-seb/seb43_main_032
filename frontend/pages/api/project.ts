87bffaf70862d619f7eb555c19abc1aa7de6cc17
shdomi8599 <shdomi@naver.com>
2023-05-14 12:13:09 +0900

    fix : project handler

--- frontend/pages/api/project.ts
+++ frontend/pages/api/project.ts
@@ -2,12 +2,12 @@ import { PROJECTS } from '@/datas/project';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  // const url = new URL(req.url!);
-  // const size = Number(url.searchParams.get('size'));
-  // const page = Number(url.searchParams.get('page'));
-  // const total = PROJECTS.length;
-  // const data = PROJECTS.slice((page - 1) * size, page * size);
-  return res.status(200).json(PROJECTS);
+  const { size, page } = req.query;
+  const sizeNum = Number(size);
+  const pageNum = Number(page);
+  const total = PROJECTS.length;
+  const data = PROJECTS.slice((pageNum - 1) * sizeNum, pageNum * sizeNum);
+  return res.status(200).json({ data, total });
 };
 
 export default handler;
