ca09bb498c6f7c3b9eceacd6abcdff700776c75a
shdomi8599 <shdomi@naver.com>
2023-05-14 12:10:31 +0900

    fix : community handler

--- frontend/pages/api/community.ts
+++ frontend/pages/api/community.ts
@@ -1,20 +1,21 @@
 import { COMMUNITY } from '@/datas/community';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // const url = new URL(req.url!);
-  // const size = Number(url.searchParams.get('size'));
-  // const page = Number(url.searchParams.get('page'));
-  // const start = (page - 1) * size;
-  // const search = url.searchParams.get('search');
-  // const end = page * size;
-  // let filteredData = COMMUNITY;
-  // if (search !== null) {
-  //   filteredData = COMMUNITY.filter((p) => p.title.includes(search));
-  // } else {
-  //   filteredData = COMMUNITY;
-  // }
-  // const data = filteredData.slice(start, end);
-  // const total = filteredData.length;
-  return res.status(200).json(COMMUNITY);
-}
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { size, page, search } = req.query;
+  const sizeNum = Number(size);
+  const pageNum = Number(page);
+  const start = (pageNum - 1) * sizeNum;
+  const end = pageNum * sizeNum;
+  let filteredData = COMMUNITY;
+  if (search) {
+    filteredData = COMMUNITY.filter((p) => p.title.includes(search as string));
+  } else {
+    filteredData = COMMUNITY;
+  }
+  const data = filteredData.slice(start, end);
+  const total = filteredData.length;
+  return res.status(200).json({ data, total });
+};
+
+export default handler;
--- frontend/pages/api/project.ts
+++ frontend/pages/api/project.ts
@@ -1,11 +1,13 @@
 import { PROJECTS } from '@/datas/project';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
   // const url = new URL(req.url!);
   // const size = Number(url.searchParams.get('size'));
   // const page = Number(url.searchParams.get('page'));
   // const total = PROJECTS.length;
   // const data = PROJECTS.slice((page - 1) * size, page * size);
   return res.status(200).json(PROJECTS);
-}
+};
+
+export default handler;
