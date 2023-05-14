ea774fffa6b7f61a520614274c97e5d1c66216e7
GiRang Choi <choigirang@kakao.com>
2023-05-14 23:19:05 +0900

    Refactor: 이미지 반응형

--- frontend/components/BannerFirst.tsx
+++ frontend/components/BannerFirst.tsx
@@ -1,37 +1,37 @@
 import { BannerProps } from '@/types/types';
 import Image from 'next/image';
-import React from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 import computer from '../public/images/bannerComputer.svg';
 import dictionary from '../public/images/bannerDic.svg';
 import message from '../public/images/bannerMsg.svg';
 import right from '../public/images/circleLeft.svg';
 import left from '../public/images/circleRight.svg';
 import square from '../public/images/squareBg.svg';
+import { useRouter } from 'next/router';
 
-export default function BannerFirst({ imgWidth }: { imgWidth: boolean }) {
+export default function BannerFirst() {
   return (
-    <ImgContainer
-      imgWidth={imgWidth}
-      // ref={imgContainerRef}
-      // showImg={showImages}
-    >
-      <div className="circleXL"></div>
-      <Image src={right} alt="img5" className="right"></Image>
-      <Image src={square} alt="img6" className="square"></Image>
-      <Image src={left} alt="img4" className="left"></Image>
-      <Image src={computer} alt="img1" className="computer"></Image>
-      <Image src={message} alt="img2" className="message"></Image>
-      <Image src={dictionary} alt="img3" className="dictionary"></Image>
-
-      <div className="rightBox">
-        <div className="textBox">
-          <span>기획부터 개발까지</span>
-          <span>사이드퀘스트</span>
+    <ImgContainer>
+      <div className="right">
+        <Image src={right} alt="img5"></Image>
+      </div>
+      <div className="square">
+        <Image src={square} alt="img6"></Image>
+      </div>
+      <div className="left">
+        <Image src={left} alt="img4"></Image>
+      </div>
+      <div className="itemBox2">
+        <div className="dictionary">
+          <Image src={dictionary} alt="img3"></Image>
         </div>
-        <button className="noto-regular-13">
-          <span className="text">프로젝트 함께하기</span>
-        </button>
+        <div className="computer">
+          <Image src={computer} alt="img1"></Image>
+        </div>
+        <div className="message">
+          <Image src={message} alt="img2"></Image>
+        </div>
       </div>
     </ImgContainer>
   );
@@ -39,191 +39,76 @@ export default function BannerFirst({ imgWidth }: { imgWidth: boolean }) {
 
 const ImgContainer = styled.div<BannerProps>`
   position: relative;
-  width: ${(props) => (props.imgWidth ? '100%' : '1280px')};
-  height: 100%;
+  width: 100%;
+  max-width: 1280px;
   margin: 0 auto;
-  /* opacity: ${(props) => (props.showImg ? 1 : 0)}; */
-  transform: translateY(${(props) => (props.showImg ? 0 : '30px')});
-  transition: opacity 0.5s ease, transform 0.5s ease;
-
-  > .circleXL {
-    position: absolute;
-    width: 10%;
-    top: 10%;
-    left: 30%;
-    opacity: 0.5;
-    border-radius: 50%;
-    background-color: transparent;
-    box-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 20px #fff, 0 0 40px #fff,
-      0 0 30px #fff, 0 0 40px #fff, 0 0 50px #fff;
-  }
-
-  > .computer {
+  height: 660px;
+  > .itemBox2 {
+    display: flex;
     position: absolute;
     top: 40%;
-    left: 13%;
-    animation: computer-move 2s ease-in-out infinite alternate;
-  }
+    width: 100%;
+    > .computer {
+      width: 34%;
+      min-width: 200px;
+      animation: computer-move 2s ease-in-out infinite alternate;
+      > img {
+        width: 100%;
+      }
+    }
 
-  > .message {
-    position: absolute;
-    top: 43%;
-    left: 45%;
-    animation: message-dictionary-move 2s ease-in-out infinite alternate;
-  }
+    > .message {
+      width: 11%;
+      min-width: 70px;
+      animation: message-dictionary-move 2s ease-in-out infinite alternate;
+      > img {
+        width: 100%;
+      }
+    }
 
-  > .dictionary {
-    position: absolute;
-    top: 68%;
-    left: 5%;
-    animation: message-dictionary-move 2s ease-in-out infinite alternate;
+    > .dictionary {
+      display: flex;
+      justify-content: end;
+      align-items: end;
+      width: 9%;
+      max-height: 250px;
+      animation: message-dictionary-move 2s ease-in-out infinite alternate;
+      > img {
+        width: 100%;
+      }
+    }
   }
 
   > .left {
+    width: 13%;
+    min-width: 150px;
     position: absolute;
     top: 20%;
     right: 0%;
+    > img {
+      width: 100%;
+    }
   }
 
   > .right {
+    width: 7%;
+    min-width: 150px;
     position: absolute;
     top: 30%;
     left: 3%;
+    > img {
+      width: 100%;
+    }
   }
 
   > .square {
+    width: 30%;
+    min-width: 200px;
     position: absolute;
     top: 20%;
     left: 27%;
-  }
-
-  > .rightBox {
-    position: absolute;
-    right: 5%;
-    bottom: 0;
-    display: flex;
-    justify-content: center;
-    align-items: end;
-    flex-direction: column;
-    width: 50%;
-    height: 100%;
-
-    > .textBox {
-      display: flex;
-      flex-direction: column;
-      align-items: end;
-      position: absolute;
-
-      > span {
-        display: inline-block;
-        font-family: 'Noto Sans', sans-serif;
-        font-weight: 500;
-        color: white;
-        font-size: 45px;
-        padding: 10px 0;
-      }
-      & span:nth-of-type(2) {
-        font-weight: bold;
-        color: hsl(261deg 80% 48%);
-        text-shadow: -1px -1px 1px rgba(255, 255, 255, 0.1),
-          1px 1px 1px rgba(0, 0, 0, 0.5);
-      }
-
-      &:first-child {
-        margin-top: 30px;
-      }
-    }
-
-    > button {
-      position: absolute;
-      top: 70%;
-      font-size: 21px;
-      padding: 0.7em 1.7em;
-      border-radius: 0.5em;
-      transition: all 0.5s ease;
-      border: none;
-      cursor: pointer;
-      letter-spacing: 3px;
-      background-color: hsl(261deg 80% 48%);
-      color: hsl(0, 0%, 100%);
-      box-shadow: rgb(93 24 220) 0px 7px 15px 0px;
-
-      > .text {
-        color: #f2f2f2;
-        font-weight: 600;
-      }
-    }
-  }
-
-  button:active {
-    letter-spacing: 3px;
-    background: linear-gradient(-45deg, #c28aff, #9f4afa, #6333ff);
-    color: hsl(0, 0%, 100%);
-    box-shadow: rgb(93 24 220) 0px 0px 0px 0px;
-    transform: translateY(1px);
-    transition: 50ms;
-  }
-
-  @keyframes computer-move {
-    0% {
-      transform: translateY(0);
-    }
-    100% {
-      transform: translateY(-30px);
-    }
-  }
-
-  @keyframes message-dictionary-move {
-    0% {
-      transform: translateY(0);
-    }
-    100% {
-      transform: translateY(60px);
-    }
-  }
-
-  @-webkit-keyframes AnimationName {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-  @-moz-keyframes AnimationName {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-  @-o-keyframes AnimationName {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-  @keyframes AnimationName {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
+    > img {
+      width: 100%;
     }
   }
 `;
