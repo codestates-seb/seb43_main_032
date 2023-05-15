fff9fa33a58c312566de0759895985f5ca69d0a7
Latemarch <dolphin0331@naver.com>
2023-05-15 19:30:48 +0900

    add project 목록 조회

---
+++ frontend/pages/api/projects/index.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import client from '@/libs/client/client';
+import withHandler from '@/libs/server/withHandler';
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    query: { page = 1, pageSize = 10 },
+  } = req;
+
+  const numPage = +page;
+  const numPageSize = +pageSize;
+
+  if (numPage < 1) {
+    return res.status(400).json({ error: 'Page must be greater than 0' });
+  }
+
+  try {
+    const projects = await client.project.findMany({
+      skip: (numPage - 1) * numPageSize,
+      take: numPageSize,
+      orderBy: { createdAt: 'desc' },
+    });
+
+    return res.status(200).json({ projects });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ error: 'An error occurred while fetching the projects' });
+  }
+}
+
+export default withHandler({ method: 'GET', handler });
