00dbbb48e0d344d20ef375de2bcfb5899ac2e322
Latemarch <dolphin0331@naver.com>
2023-05-15 17:39:40 +0900

    feat: add usePostApi.ts
    - add usePostApi hook

---
+++ frontend/components/authAction/usePostApi.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+interface IObj {
+  isLoading: boolean;
+  data: undefined | any;
+  error: undefined | any;
+}
+
+export default function useApi(url: string): [(data?: any) => void, IObj] {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<undefined | any>(undefined);
+  const [error, setError] = useState<undefined | any>(undefined);
+  function mutation(data?: any) {
+    setIsLoading(true);
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }) //
+      .then((res) => res.json())
+      .then(setData)
+      .catch(setError)
+      .finally(() => setIsLoading(false));
+  }
+  return [mutation, { isLoading, data, error }];
+}
