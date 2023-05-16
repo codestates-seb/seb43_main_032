96b601929ec535edf60def3ca7d02d3216e4585a
shdomi8599 <shdomi@naver.com>
2023-05-16 20:22:04 +0900

    refactor : Home spring api 연동

--- frontend/hooks/react-query/useCommunity.tsx
+++ frontend/hooks/react-query/useCommunity.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from 'react-query';
 import { api } from '@/util/api';
 import { useRouter } from 'next/router';
+import { PageInfo } from '@/types/types';
 
 type Props = {
   address: string;
@@ -11,7 +12,7 @@ export const useCommunity = <T extends {}>({ address, queryKey }: Props) => {
   const router = useRouter();
   const { isLoading, error, data, refetch } = useQuery<{
     data: T;
-    total?: number;
+    pageInfo: PageInfo;
   }>(queryKey, async () => {
     if (!router.route.includes('create')) {
       return await api(address).then((res) => res.data);
