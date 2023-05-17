2d01947c316d92f02806fca34dee4fe4487d2bb4
GiRang Choi <choigirang@kakao.com>
2023-05-18 00:19:35 +0900

    feat: api 주석해제

--- frontend/util/api/index.ts
+++ frontend/util/api/index.ts
@@ -1,12 +1,10 @@
 import axios from 'axios';
 import { getCookie } from '../cookie';
 
-// const baseURL =
-//   process.env.NODE_ENV === 'production'
-//     ? process.env.NEXT_RESOURCE_URL
-//     : 'http://13.209.14.135:8080';
-
-const baseURL = 'http://13.209.14.135:8080';
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.NEXT_RESOURCE_URL
+    : 'http://13.209.14.135:8080';
 
 const api = axios.create({
   baseURL,
