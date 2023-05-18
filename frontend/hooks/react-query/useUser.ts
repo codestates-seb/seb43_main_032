0428ee677861bab33701593513073bb485e17f5f
Latemarch <dolphin0331@naver.com>
2023-05-18 11:14:19 +0900

    .

--- frontend/hooks/react-query/useUser.ts
+++ frontend/hooks/react-query/useUser.ts
@@ -1,16 +1,81 @@
-import { getUserById, getUsers } from '@/util/api/user';
+import { api } from '@/util/api';
 import axios from 'axios';
 import { useQuery } from 'react-query';
+// import { useRecoilState, useSetRecoilState } from 'recoil';
 
-export default function useUser() {
-  // const queryClient = useQueryClient();
-  const userQuery = useQuery(['users'], () => getUsers(), {
-    staleTime: 1000 * 60 * 5,
-  });
-  const getUser = (id: number) =>
-    useQuery(['users', id], () => getUserById(id), {
-      staleTime: 1000 * 60 * 5,
-    });
-
-  return { userQuery, getUser };
+interface IProps {
+  id?: number | undefined;
+  keyword?: string | undefined;
+  page?: number;
+  pageSize?: number;
 }
+
+export default function useUser({ id, keyword, page, pageSize }: IProps) {
+  // const setIsLoggedIn = useSetRecoilState(userStatus);
+  // const queryClient = useQueryClient();
+
+  const userQuery = useQuery(['users'], () => getUsers(page, pageSize));
+
+  // const getUserStatus = useQuery(['loggedIn'], getStatus);
+  // const getMyInfo = useQuery(['users', 'me'], getMe);
+  // const setUserLogOut = useMutation(logOut, {
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries(['loggedIn']);
+  //     setIsLoggedIn(false);
+  //   },
+  // });
+
+  const getUserById = useQuery(['loggedIn', id], () => getUser(id));
+
+  const searchUserByKeyword = useQuery(['users', keyword], () =>
+    searchUser(keyword)
+  );
+
+  return {
+    userQuery, //
+    getUserById,
+    // getUserStatus,
+    // setUserLogOut,
+    // getMyInfo,
+    searchUserByKeyword,
+  };
+}
+
+// async function getStatus(): Promise<boolean> {
+//   const response = await axios.get('/api/users/status');
+//   return response.data.ok;
+// }
+async function getUsers(page?: number, pageSize?: number) {
+  const response = await fetch(
+    'https://2e7e-1-228-217-180.ngrok-free.app/member/findAll?page=1&size=12'
+  );
+  return response;
+}
+// async function getMe() {
+//   const response = await axios.get('/api/users/me');
+//   return response.data;
+// }
+
+// const logOut = async () => {
+//   const response = await fetch('/api/users/logout', {
+//     method: 'POST',
+//   });
+
+//   if (!response.ok) {
+//     throw new Error('Logout failed');
+//   }
+// };
+const getUser = async (id: number | undefined) => {
+  if (!id) return;
+  const response = await axios.get(`/api/users/${id}`);
+  return response.data;
+};
+
+export const searchUser = async (keyword: string | undefined) => {
+  // if (!name) throw new Error('ID is undefined');
+  if (!keyword) return;
+  const response = await axios.get(`/api/users/search`, {
+    params: { keyword },
+  });
+  return response.data;
+};
--- frontend/pages/users/index.tsx
+++ frontend/pages/users/index.tsx
@@ -1,10 +1,9 @@
 import GridBox from '@/components/GridBox';
 import UserCard from '@/components/user/UserCard';
 import UserSideBar from '@/components/user/UserSideBar';
-import { USERS } from '@/dummy/users';
 import useUser from '@/hooks/react-query/useUser';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 //유저 페이지 입니다. 경로 '/user/'
 
@@ -28,7 +27,13 @@ const CardWrapper = styled.div`
   } */
 `;
 const User = () => {
+  const [page, setPage] = useState<number>(1);
+  const [size, setSize] = useState<number>(12);
+  const {
+    userQuery: { data: users },
+  } = useUser({ page, pageSize: size });
   const router = useRouter();
+
   useEffect(() => {
     window.scrollTo({
       top: 670,
@@ -36,9 +41,7 @@ const User = () => {
       behavior: 'smooth',
     });
   }, [router]);
-  // const {
-  //   userQuery: { data: users },
-  // } = useUser();
+
   return (
     <GridBox>
       <UserSideBar />
