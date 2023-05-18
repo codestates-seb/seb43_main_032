ee9b4d6779eacf65c14f22380f26ba6a4e9d975e
tree4553 <tree4553@naver.com>
2023-05-18 18:51:03 +0900

    refactoring : image update api 주소 수정

--- backend/SideQuest/src/main/java/com/main_032/SideQuest/image/controller/ImageController.java
+++ backend/SideQuest/src/main/java/com/main_032/SideQuest/image/controller/ImageController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/v1/images")
+@RequestMapping("/images")
 public class ImageController {
     private final ImageUploader imageUploader;
 
