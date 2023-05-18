6894848d324d1efdde18cc43565ed554c2506cda
GiRang Choi <choigirang@kakao.com>
2023-05-18 10:44:59 +0900

    feat: stack 선택 벨 추가

--- frontend/components/stack/Stack.tsx
+++ frontend/components/stack/Stack.tsx
@@ -13,18 +13,20 @@ const Stack = ({ tech, addStack, stacks }: Props) => {
   //모달
   const [modal, setModal] = useState(false);
 
+  console.log('stacks =>', stacks);
+  console.log('tech =>', tech);
+
   return (
     <Box
       onMouseEnter={() => setModal(true)}
       onMouseLeave={() => setModal(false)}
       onClick={() => addStack && addStack(tech)}
       key={tech}
-      className={
-        stacks && stacks.filter((stack) => stack.tech === tech).length > 0
-          ? `focus bg-${tech}`
-          : `bg-${tech}`
-      }
+      className={`bg-${tech}`}
     >
+      {stacks && stacks.some((el) => el.tech === tech) && (
+        <div className="ball"></div>
+      )}
       {modal && <Bubble tech={tech} />}
     </Box>
   );
@@ -38,4 +40,14 @@ const Box = styled.li`
   min-width: 24px;
   min-height: 24px;
   box-shadow: var(--box-shadow);
+
+  .ball {
+    position: absolute;
+    width: 10px;
+    height: 10px;
+    left: -5px;
+    top: -5px;
+    border-radius: 50%;
+    background-color: #ff5454;
+  }
 `;
