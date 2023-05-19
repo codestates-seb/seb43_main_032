5bfc42d0c13d231ea7ff326b3f4ed0663852a286
GiRang Choi <choigirang@kakao.com>
2023-05-19 10:20:27 +0900

    feat: 카테고리 구조 변경

--- frontend/components/community/ViewCommunity.tsx
+++ frontend/components/community/ViewCommunity.tsx
@@ -10,6 +10,12 @@ import Btn from '../button/Btn';
 import { useCommunity } from '@/hooks/react-query/useCommunity';
 import { Community } from '@/types/community';
 import CommentBox from '../CommentBox';
+import GridBox from '../GridBox';
+import Position from '../Position';
+import Tag from '../Tag';
+import { useRecoilValue } from 'recoil';
+import { loggedInUserState } from '@/recoil/atom';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 
 const ReactMarkdown = dynamic(() => import('@/components/editor/ContentBox'), {
   ssr: false,
@@ -28,11 +34,17 @@ const ViewCommunity = () => {
   const address = `/articles/${id}`;
   const queryKey = ['articles', 'post', id];
 
+  // 임시 데이터
+  const loggedInUser = useRecoilValue(loggedInUserState);
+  const project = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const [userHeart, setUserHeart] = useState(false);
+
   const { communityQuery } = useCommunity<Community>({
     address,
     queryKey,
   });
   const data = communityQuery.data?.data;
+  console.log(data);
 
   //댓글 관련
   const [commentVal, setCommentVal] = useState('');
@@ -54,30 +66,72 @@ const ViewCommunity = () => {
   if (communityQuery.error)
     return <Message>잠시 후 다시 시도해주세요.</Message>;
   return (
-    <Container>
+    <GridBox>
       {data && (
         <>
           <Top>
-            <div className="left">
-              <div className="title">{data.title}</div>
-              <div className="date">{data.createdAt}</div>
-            </div>
             <div className="right">
-              <img src={data.memberInfo.profileImageUrl}></img>
-              <div className="userBox nanum-bold userStar">
-                <FaHeart color="red" /> {data.totalLikes}
+              <div className="top-box">
+                <img src={data.memberInfo.profileImageUrl}></img>
+                <div className="userBox nanum-bold userName">
+                  {data.memberInfo.name}
+                </div>
+                {data.memberInfo.email !== loggedInUser?.email && (
+                  <div
+                    className="saveStar"
+                    onClick={() => setUserHeart(!userHeart)}
+                  >
+                    <span className="icon-box">
+                      {userHeart ? (
+                        <AiOutlineHeart fill={'#ececec'} />
+                      ) : (
+                        <AiFillHeart fill="red" />
+                      )}
+                    </span>
+                  </div>
+                )}
               </div>
-              <div className="userBox nanum-bold userMail">
-                {data.memberInfo.email}
+              <div className="detail-box">
+                <div className="detail-sub-box">
+                  <div className="detail-num">
+                    {project.length} <span>개</span>
+                  </div>
+                  <div className="detail-title">진행 프로젝트</div>
+                </div>
+                <div className="center-border"></div>
+                <div className="detail-sub-box">
+                  <div className="detail-num">
+                    {data.totalLikes} <span>개</span>
+                  </div>
+                  <div className="detail-title">평가 점수</div>
+                </div>
               </div>
+              {data.memberInfo.email !== loggedInUser?.email && (
+                <Tag>쪽지 보내기</Tag>
+              )}
             </div>
           </Top>
           <Bottom>
+            <div className="main-title">
+              <div className="title">{data.title}</div>
+            </div>
+            <div className="sub-title">
+              <div className="date">
+                <span className="writeDate">작성일 : </span>
+                {data.createdAt.slice(0, 10)}
+              </div>
+
+              <div className="view">
+                <span className="viewNum">조회수 : </span>
+                {data.view}
+              </div>
+
+              <div className="comment">
+                <span className="commentNum">댓글수 : </span>
+              </div>
+            </div>
             <div className="content">
-              <ReactMarkdown
-                content={data.content}
-                backColor="white"
-              ></ReactMarkdown>
+              <ReactMarkdown content={data.content}></ReactMarkdown>
               <CommentBox
                 commentPage={commentPage}
                 commentPageHandler={commentPageHandler}
@@ -90,131 +144,120 @@ const ViewCommunity = () => {
           </Bottom>
         </>
       )}
-    </Container>
+    </GridBox>
   );
 };
 
 export default ViewCommunity;
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 20px;
-  color: #fff;
-
-  &:first-child {
-    padding-left: 0;
-    padding-right: 0;
-  }
-`;
-
 const Top = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  flex-direction: column;
   width: 100%;
   padding: 20px;
-  border-radius: 15px;
-  background: linear-gradient(-45deg, #ffc6fd, #e812c1, #7402e0);
-  background-size: 400% 400%;
-  animation: AnimationName 15s ease infinite;
-  width: 100%;
-  top: 35%;
-  text-align: center;
   margin-bottom: 20px;
 
-  > .left {
-    height: 150px;
-    display: flex;
-    flex-direction: column;
-    justify-content: end;
-    align-items: start;
-    font-size: 50px;
-    color: white;
-
-    > .title {
-      margin-bottom: 20px;
-    }
-
-    > .date {
-      font-size: 15px;
-    }
-  }
   > .right {
-    height: 150px;
     display: flex;
     flex-direction: column;
-    justify-content: end;
-    align-items: end;
+    justify-content: center;
+    border-radius: 15px;
+    align-items: center;
+    padding: 40px 30px 20px;
+    border: solid 2px #ececec;
 
-    > img {
-      width: 100px;
-      height: 100px;
-      border: solid 2px gray;
-      border-radius: 50%;
-      background-color: white;
+    .tag {
+      margin-top: 30px;
     }
 
-    > .userBox {
+    .top-box {
+      width: 100%;
+      position: relative;
+      > img {
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+        background-color: white;
+        box-shadow: 0px 0px 11px 11px rgba(234, 234, 234, 0.77);
+        margin-bottom: 20px;
+      }
+
+      > .userBox {
+        display: flex;
+        align-items: center;
+        gap: 16px;
+        padding: 10px;
+        padding-bottom: 4px;
+        font-size: 15px;
+        font-weight: bold;
+      }
+
+      > .userName {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        padding-top: 0;
+        border-bottom: solid 2px #ececec;
+        padding-bottom: 20px;
+        font-size: 18px;
+        color: #9f9f9f;
+      }
+
+      > .saveStar {
+        position: absolute;
+        width: 50px;
+        height: 50px;
+        background-color: #cecece;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        top: 0;
+        right: 0;
+        cursor: pointer;
+
+        > .icon-box {
+          width: 50%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 30px;
+        }
+      }
+    }
+
+    > .detail-box {
       display: flex;
-      align-items: center;
-      gap: 16px;
-      padding: 10px;
-      padding-bottom: 4px;
-      font-size: 15px;
-      font-weight: bold;
-      color: white;
-    }
+      width: 80%;
+      justify-content: space-between;
+      margin-bottom: 24px;
 
-    > .userMail {
-      padding-top: 0;
-    }
-  }
+      > .center-border {
+        width: 1px;
+        height: 150%;
+        border: solid 1px #ececec;
+      }
 
-  @-webkit-keyframes AnimationName {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-  @-moz-keyframes AnimationName {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-  @-o-keyframes AnimationName {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-  @keyframes AnimationName {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
+      > .detail-sub-box {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        > .detail-num {
+          padding: 10px;
+          font-size: 24px;
+
+          > span {
+            font-size: 15px;
+            color: #828282;
+          }
+        }
+
+        > .detail-title {
+          font-size: 12px;
+        }
+      }
     }
   }
 `;
@@ -222,12 +265,13 @@ const Top = styled.div`
 const Bottom = styled.div`
   width: 100%;
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
   font-size: 20px;
+  padding: 20px 20px 36px;
+  gap: 32px;
 
   > .content {
-    width: 68%;
-    position: relative;
+    width: 100%;
     display: flex;
     flex-direction: column;
     gap: 40px;
@@ -236,7 +280,6 @@ const Bottom = styled.div`
   > .comment {
     display: flex;
     flex-direction: column;
-    width: 30%;
 
     > .each {
       color: black;
@@ -248,6 +291,29 @@ const Bottom = styled.div`
         margin-top: 5px;
         border-radius: 10px;
       }
+    }
+  }
+
+  .main-title {
+    display: flex;
+    flex-direction: column;
+
+    .title {
+      font-size: 27px;
+      font-weight: 700;
+      border-bottom: solid 1px #ececec;
+      padding-bottom: 10px;
+    }
+  }
+  .sub-title {
+    color: #9f9f9f;
+    font-size: 13px;
+    display: flex;
+    gap: 32px;
+
+    .writeDate,
+    .viewNum {
+      font-weight: 600;
     }
   }
 `;
