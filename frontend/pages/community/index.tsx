9fadc5432ed28a00229ca2a39cc3342eaa0081c9
GiRang Choi <choigirang@kakao.com>
2023-05-19 10:20:58 +0900

    feat: 구조변경

--- frontend/pages/community/index.tsx
+++ frontend/pages/community/index.tsx
@@ -1,14 +1,33 @@
 import GridBox from '@/components/GridBox';
 import Content from '@/components/community/Content';
 import SideBar from '@/components/community/SideBar';
+import styled from 'styled-components';
 
 const Community = () => {
   return (
-    <GridBox>
+    <Box>
       <SideBar />
       <Content />
-    </GridBox>
+    </Box>
   );
 };
 
 export default Community;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  grid-area: auto;
+`;
+
+const Box = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 0 20px 10px;
+  @media (max-width: 960px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
