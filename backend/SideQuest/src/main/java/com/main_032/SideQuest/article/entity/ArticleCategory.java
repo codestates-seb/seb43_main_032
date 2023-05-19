febf80eb0a2bb05c4c55438f89ae52c96245aa93
tree4553 <tree4553@naver.com>
2023-05-19 18:08:26 +0900

    feat : Article Category 내용 추가

--- backend/SideQuest/src/main/java/com/main_032/SideQuest/article/entity/ArticleCategory.java
+++ backend/SideQuest/src/main/java/com/main_032/SideQuest/article/entity/ArticleCategory.java
@@ -1,9 +1,17 @@
 package com.main_032.SideQuest.article.entity;
 
 public enum ArticleCategory {
-    FRONT_END("프론트"),
-    BACK_END("백엔드"),
-    UI_UX("UI/UX");
+    FRONTEND("프론트"),
+    BACKEND("백엔드"),
+    UIUX("UI/UX"),
+    PLANNING("기획"),
+    DESIGNER("디자이너"),
+    PM("PM"),
+    BUSINESS("사업기획"),
+    MARKETING("마케팅"),
+    ANDROID("안드로이드"),
+    IOS("IOS"),
+    OTHER("기타");
     private final String displayName;
 
     ArticleCategory(String displayName) {
@@ -14,3 +22,14 @@ public enum ArticleCategory {
         return displayName;
     }
 }
+//'프론트엔드',
+//        '백엔드',
+//        'UI/UX',
+//        '기획',
+//        '디자이너',
+//        'PM',
+//        '사업기획',
+//        '마케팅',
+//        '안드로이드',
+//        'IOS',
+//        '기타',
