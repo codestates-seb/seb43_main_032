874f91e243750cad8164e176bf458b0e25f19780
Latemarch <dolphin0331@naver.com>
2023-05-20 14:20:42 +0900

    feat: update edit page enable updating of user profile file

--- frontend/pages/users/me/edit.tsx
+++ frontend/pages/users/me/edit.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { FieldErrors, useForm } from 'react-hook-form';
 import styled from 'styled-components';
 import useUser from '@/hooks/react-query/useUser';
@@ -7,6 +7,7 @@ import EditInput from '@/components/authAction/EditInput';
 import { api } from '@/util/api';
 import Btn from '@/components/button/Btn';
 import GridBox from '@/components/GridBox';
+import uploadFile from '@/util/api/uploadFile';
 
 const SideBar = styled.div`
   background-color: rgba(0, 0, 0, 0.1);
@@ -30,15 +31,32 @@ const ImgWrapper = styled.div`
   flex-direction: column;
   width: 200px;
   height: 200px;
-  border-radius: 50%;
+  border-radius: 20px;
   overflow: hidden;
   margin-right: 80px;
   margin-top: 30px;
   background-color: #cbcbcb;
 
+  div {
+    position: absolute;
+    bottom: 10px;
+    width: 100%;
+    height: 20px;
+  }
   input {
-    border: 1px solid red;
-    opacity: 0;
+    display: none;
+  }
+  label {
+    display: flex;
+    justify-content: center;
+    font-size: 20px;
+    padding: 5px;
+    width: 100%;
+    border: none;
+    background-color: skyblue;
+  }
+  img {
+    object-fit: cover;
     width: 100%;
     height: 100%;
   }
@@ -70,24 +88,51 @@ const Button = styled.button.attrs({ className: 'nanum-bold' })`
   background-color: skyblue;
 `;
 
-interface ISubmit {
-  [key: string]: string;
-}
+const dummyUser = {
+  aboutMe: 'string',
+  email: 'string',
+  location: 'string',
+  memberId: 0,
+  name: 'string',
+  phone: 'string',
+  position: 'string',
+  profileImageUrl: 'string',
+  techList: [
+    {
+      tech: 'string',
+    },
+  ],
+  totalStar: 0,
+  yearOfDev: 0,
+};
 export default function Edit() {
-  const {
-    getMyInfo: { data: user },
-  } = useUser({});
-  const { register, handleSubmit } = useForm();
+  const user = dummyUser;
+  // const {
+  //   getMyInfo: { data: user },
+  // } = useUser({});
+  const { register, handleSubmit, watch } = useForm();
+  const [imgPreview, setImgPreview] = useState('');
+  const image = watch('image');
 
-  const onValid = (data: ISubmit) => {
-    console.log(data);
+  const onValid = async (data: any) => {
+    if (image && image.length > 0) {
+      const profileImageUrl = (await uploadFile(data.image[0]))[0];
+      data.profileImageUrl = profileImageUrl;
+    }
+    delete data.image;
+
+    const filteredData = Object.fromEntries(
+      Object.entries(data).filter(([key, value]) => {
+        return !(value === '' || (Array.isArray(value) && value.length === 0));
+      })
+    );
+
     const updatedData = {
       ...data,
-      techList: [''],
-      profileImageUrl: '',
-      yearOfDev: +data.yearOfDev,
+      ...filteredData,
     };
     console.log(updatedData);
+
     api
       .patch('/members', updatedData) //
       .then((res) => console.log(res));
@@ -104,22 +149,32 @@ export default function Edit() {
       behavior: 'smooth',
     });
   }, [router]);
+  useEffect(() => {
+    if (image && image.length > 0) {
+      setImgPreview(URL.createObjectURL(image[0]));
+      console.log(image);
+    }
+  }, [image]);
 
   return (
     <GridBox>
       <SideBar></SideBar>
       <Wrapper>
         {user && (
-          <Form
-            onSubmit={handleSubmit(onValid, onInValid)}
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-            }}
-          >
+          <Form onSubmit={handleSubmit(onValid, onInValid)}>
             <ProfileBox>
-              <ImgWrapper />
+              <ImgWrapper>
+                <img src={imgPreview} />
+                <div>
+                  <label htmlFor="image">change file</label>
+                  <input
+                    {...register('image')}
+                    id="image"
+                    type="file"
+                    accept="image/*"
+                  />
+                </div>
+              </ImgWrapper>
               <div className="nameBox">
                 <EditInput
                   label="NAME"
