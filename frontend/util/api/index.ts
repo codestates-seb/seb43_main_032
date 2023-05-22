31cf6f946362920f6afd2b19d62babc38593ad5c
shdomi8599 <shdomi@naver.com>
2023-05-23 02:17:11 +0900

    test : deploy

--- frontend/util/api/index.ts
+++ frontend/util/api/index.ts
@@ -3,13 +3,13 @@ import { getCookie } from '../cookie';
 
 axios.defaults.withCredentials = true;
 
-const baseURL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.NEXT_RESOURCE_URL
-    : 'http://3.36.211.36:8080';
+// const baseURL =
+//   process.env.NODE_ENV === 'production'
+//     ? process.env.NEXT_RESOURCE_URL
+//     : 'https://side-quest-1.com';
 
 const api = axios.create({
-  baseURL,
+  baseURL: 'https://side-quest-1.com',
   timeout: 3000,
   withCredentials: true,
   headers: {
