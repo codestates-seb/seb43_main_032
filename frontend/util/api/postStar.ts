17a77d74d3ab8dea890884bb021e848c5011dc6e
shdomi8599 <shdomi@naver.com>
2023-05-22 22:11:47 +0900

    feat : postStar 함수

---
+++ frontend/util/api/postStar.ts
@@ -0,0 +1,14 @@
+import { api } from '.';
+
+/**
+ * 멤버의 별 점수를 올려주는 이벤트
+ * @param memberId 별 점수를 주고 싶은 멤버의 ID
+ * @param star 주고싶은 별의 개수
+ */
+export const postStar = (memberId: number, star: number) => {
+  const data = {
+    memberId,
+    star,
+  };
+  api.post(`/members/stars`, data);
+};
