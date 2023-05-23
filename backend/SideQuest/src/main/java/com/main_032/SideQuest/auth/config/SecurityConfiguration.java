70444f7dbb07c59e0f7ad3df2c9355435e967f66
tree4553 <tree4553@naver.com>
2023-05-24 03:28:13 +0900

    feat : OAuth2 Google 구현

--- backend/SideQuest/src/main/java/com/main_032/SideQuest/auth/config/SecurityConfiguration.java
+++ backend/SideQuest/src/main/java/com/main_032/SideQuest/auth/config/SecurityConfiguration.java
@@ -3,9 +3,11 @@ package com.main_032.SideQuest.auth.config;
 import com.main_032.SideQuest.auth.filter.JwtAuthenticationFilter;
 import com.main_032.SideQuest.auth.filter.JwtVerificationFilter;
 import com.main_032.SideQuest.auth.handler.MemberAuthenticationSuccessHandler;
+import com.main_032.SideQuest.auth.handler.OAuth2SuccessHandler;
 import com.main_032.SideQuest.auth.jwt.JwtTokenizer;
 import com.main_032.SideQuest.auth.utils.CustomAuthorityUtils;
 import com.main_032.SideQuest.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -16,20 +18,16 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
     private final JwtTokenizer jwtTokenizer;
     private final CustomAuthorityUtils authorityUtils;
     private final MemberRepository memberRepository;
-
-    public SecurityConfiguration(JwtTokenizer jwtTokenizer, CustomAuthorityUtils authorityUtils, MemberRepository memberRepository) {
-        this.jwtTokenizer = jwtTokenizer;
-        this.authorityUtils = authorityUtils;
-        this.memberRepository = memberRepository;
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -43,38 +41,16 @@ public class SecurityConfiguration {
                 .formLogin().disable()
                 .httpBasic().disable()
                 .exceptionHandling()
-//                .authenticationEntryPoint(new MemberAuthenticationEntryPoint())  // 추가
-//                .accessDeniedHandler(new MemberAccessDeniedHandler())            // 추가
                 .and()
                 .apply(new CustomFilterConfigure())
                 .and()
                 .authorizeHttpRequests(authorize -> authorize
-                                .antMatchers(HttpMethod.POST, "/*/login").permitAll()
-                                .antMatchers(HttpMethod.POST, "/*/signup").permitAll()
-
-//                        .antMatchers(HttpMethod.PATCH, "/*/member").hasRole("USER")
-//                        .antMatchers(HttpMethod.PATCH, "/*/member/**").hasRole("ADMIN")
-//                        .antMatchers(HttpMethod.GET, "/*/member").hasRole("USER")
-//                        .antMatchers(HttpMethod.GET, "/*/member/**").hasRole("ADMIN")
-//                        .antMatchers(HttpMethod.DELETE, "/*/member").hasRole("USER")
-//                        .antMatchers(HttpMethod.DELETE, "/*/member/**").hasRole("ADMIN")
-//
-//                        .antMatchers(HttpMethod.POST, "/*/ask").hasRole("USER")
-//                        .antMatchers(HttpMethod.PATCH, "/*/ask/**").hasRole("USER")
-//                        .antMatchers(HttpMethod.GET, "/*/ask").permitAll()
-//                        .antMatchers(HttpMethod.GET, "/*/ask/**").permitAll()
-//                        .antMatchers(HttpMethod.DELETE, "/*/ask/**").hasAnyRole("USER", "ADMIN")
-//
-//                        .antMatchers(HttpMethod.POST, "/*/answer").hasRole("USER")
-//                        .antMatchers(HttpMethod.PATCH, "/*/answer/**").hasRole("USER")
-//                        .antMatchers(HttpMethod.GET, "/*/answer/**").permitAll()
-//                        .antMatchers(HttpMethod.DELETE, "/*/answer/**").hasAnyRole("USER", "ADMIN")
-//
-//                        .antMatchers(HttpMethod.POST, "/*/comment").hasRole("USER")
-//                        .antMatchers(HttpMethod.PATCH, "/*/comment/**").hasRole("USER")
-//                        .antMatchers(HttpMethod.GET, "/*/comment/**").permitAll()
-//                        .antMatchers(HttpMethod.DELETE, "/*/comment/**").hasAnyRole("USER", "ADMIN")
+                        .antMatchers(HttpMethod.POST, "/*/login").permitAll()
+                        .antMatchers(HttpMethod.POST, "/*/signup").permitAll()
                         .anyRequest().permitAll()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(new OAuth2SuccessHandler(jwtTokenizer, authorityUtils, memberRepository))
                 );
         return http.build();
     }
@@ -92,13 +68,12 @@ public class SecurityConfiguration {
             JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
             jwtAuthenticationFilter.setFilterProcessesUrl("/members/login");
             jwtAuthenticationFilter.setAuthenticationSuccessHandler(new MemberAuthenticationSuccessHandler(memberRepository));
-//            jwtAuthenticationFilter.setAuthenticationFailureHandler(new MemberAuthenticationFailureHandler());
-
             JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, authorityUtils);
 
             builder
                     .addFilter(jwtAuthenticationFilter)
-                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
+//                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
+                    .addFilterAfter(jwtVerificationFilter, OAuth2LoginAuthenticationFilter.class);
         }
     }
 }
