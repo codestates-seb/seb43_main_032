038a3a12c46f1a1a0f172e370315438f312de05d
shdomi8599 <shdomi@naver.com>
2023-05-25 01:03:00 +0900

    refactor : 채팅 데이터 recoil 관리

--- frontend/pages/chat/[id].tsx
+++ frontend/pages/chat/[id].tsx
@@ -1,20 +1,16 @@
 import SubBtn from '@/components/button/SubBtn';
 import ChatBox from '@/components/common_box/ChatBox';
-import { loggedInUserState } from '@/recoil/atom';
-import { Chat } from '@/types/user';
+import { chatState } from '@/recoil/atom';
 import { formatDate2 } from '@/util/date';
 import { useRouter } from 'next/router';
-import { useQueryClient } from 'react-query';
 import { useRecoilValue } from 'recoil';
 
 import styled from 'styled-components';
 const ChatItem = () => {
   const router = useRouter();
   const { id } = router.query;
-  const userId = useRecoilValue(loggedInUserState);
-  const cache = useQueryClient();
-  const data = cache.getQueryData(['chat-data', userId]) as { data: Chat[] };
-  const item = data.data.find((chat) => chat.id === Number(id));
+  const chat = useRecoilValue(chatState);
+  const item = chat.find((data) => data.id === Number(id));
   const backEvent = () => {
     router.back();
   };
