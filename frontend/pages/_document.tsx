99614a74c0235c86a9f3737d66013963e05f2230
shdomi8599 <shdomi@naver.com>
2023-05-26 12:21:56 +0900

    docs : meta 요소 수정

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -10,14 +10,23 @@ const MyDocument = ({ styles }: Props) => {
   return (
     <Html lang="ko">
       <Head>
-        <meta name="description" content="Side-Quest입니다." />
+        <meta
+          name="description"
+          content="개발자와 디자이너 등 다양한 색을 가진 각 분야의 전문가들이 모여 더 멋진 앞날을 꿈꾸는 곳입니다."
+        />
         <meta property="og:title" content="Side-Quest" />
-        <meta property="og:description" content="Side-Quest입니다." />
+        <meta
+          property="og:description"
+          content="개발자와 디자이너 등 다양한 색을 가진 각 분야의 전문가들이 모여 더 멋진 앞날을 꿈꾸는 곳입니다."
+        />
         <meta property="og:image" content="/images/logo.svg" />
         <meta property="og:url" content="https://sidequest.co.kr/" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content="Side-Quest" />
-        <meta name="twitter:description" content="Side-Quest입니다." />
+        <meta
+          name="twitter:description"
+          content="개발자와 디자이너 등 다양한 색을 가진 각 분야의 전문가들이 모여 더 멋진 앞날을 꿈꾸는 곳입니다."
+        />
         <meta name="twitter:image" content="/images/logo.svg" />
         <link rel="canonical" href="https://sidequest.co.kr/" />
         <link rel="icon" href="/favicon.png" />
