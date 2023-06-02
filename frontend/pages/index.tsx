c5ef61cdd3ce162d4b8dffaee1fc7a5692a2a117
shdomi8599 <shdomi@naver.com>
2023-06-03 00:20:06 +0900

    refactor : 홈 페이지 SSR 적용

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,77 +1,79 @@
 import styled from 'styled-components';
 import ProjectCardBox from '@/components/card_box/ProjectCardBox';
 import CommunityCardBox from '@/components/card_box/CommunityCardBox';
-import ProjectSkeleton from '@/components/skeleton/ProjectSkeleton';
-import CommunityItemSkeleton from '@/components/skeleton/CommunityItemSkeleton';
-import { useTopData } from '@/hooks/react-query/useTopData';
 import { useState } from 'react';
 import UserCardBox from '@/components/card_box/UserCardBox';
-import UserItemSkeleton from '@/components/skeleton/UserItemSkeleton';
 import Head from 'next/head';
-import Message from '@/components/Message';
+import { api } from '@/util/api';
+import { MemberInfo } from '@/types/types';
 
-const Home = () => {
-  const {
-    topLikeProjectData,
-    topViewProjectData,
-    topViewCommunityData,
-    topLikeCommunityData,
-    topMembersData,
-    topViewcommunityQuery,
-    topLikeProjectLoading,
-    topViewProjectLoading,
-    isLoadingMembers,
-    checkError,
-  } = useTopData();
-
+const Home = ({
+  topLikeProjectData,
+  topViewProjectData,
+  topViewCommunityData,
+  topLikeCommunityData,
+  topMembersData,
+}: any) => {
   const [communityFilter, setCommunityFilter] = useState(0);
   const filterHandler = (idx: number) => {
     setCommunityFilter(idx);
   };
   const community = [topViewCommunityData, topLikeCommunityData];
 
-  if (checkError) return <Message>잠시 후에 다시 시도해주세요.</Message>;
   return (
     <>
       <Head>
         <title>SIDE QUEST</title>
       </Head>
       <Box>
+        <ProjectCardBox data={topLikeProjectData} title={'인기 프로젝트'} />
         <ProjectCardBox
-          skeleton={topLikeProjectLoading && <ProjectSkeleton />}
-          data={topLikeProjectData ? topLikeProjectData : []}
-          title={'인기 프로젝트'}
-        />
-        <ProjectCardBox
-          skeleton={topViewProjectLoading && <ProjectSkeleton />}
-          data={topViewProjectData ? topViewProjectData : []}
+          data={topViewProjectData}
           title={'주목 중인 프로젝트'}
         />
         <CommunityCardBox
-          skeleton={
-            topViewcommunityQuery.isLoading && (
-              <CommunityItemSkeleton
-                isHome={true}
-                width="100%"
-                gap="8px"
-                count={5}
-              />
-            )
-          }
           filterHandler={filterHandler}
           selected={communityFilter}
           data={community[communityFilter]}
         />
-        <UserCardBox
-          skeleton={isLoadingMembers && <UserItemSkeleton count={5} />}
-          data={topMembersData ? topMembersData : []}
-        />
+        <UserCardBox data={topMembersData} />
       </Box>
     </>
   );
 };
 
 export default Home;
+
+export async function getServerSideProps() {
+  const topLikeProjectData = await api('/projects/likes-top5').then((res) =>
+    res.data.data.projectList.slice(0, 4)
+  );
+  const topViewProjectData = await api('/projects/views-top5').then((res) =>
+    res.data.data.projectList.slice(0, 4)
+  );
+  const topViewCommunityData = await api('/articles/view-top5').then(
+    (res) => res.data.data.articleList
+  );
+  const topLikeCommunityData = await api('/articles/likes-top5').then(
+    (res) => res.data.data.articleList
+  );
+  const topMembersData = await api('/members/find-all?page=1&size=1000').then(
+    (res) =>
+      res.data.data
+        ?.sort((x: MemberInfo, y: MemberInfo) => y.totalStar - x.totalStar)
+        .slice(0, 5)
+  );
+
+  return {
+    props: {
+      topLikeProjectData,
+      topViewProjectData,
+      topViewCommunityData,
+      topLikeCommunityData,
+      topMembersData,
+    },
+  };
+}
 
 const Box = styled.div`
   width: 100%;
